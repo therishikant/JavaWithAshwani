265156e769a5b697f9d08f7e718f9a8e1172a0ec
ashwthama <ashwanikumarnt@gmail.com>
2021-11-06 23:21:41 +0530

    kth element testing

--- src/com/ashwani/main/Driver.java
+++ src/com/ashwani/main/Driver.java
@@ -1,11 +1,13 @@
 package com.ashwani.main;
 
+
 import com.ashwani.utils.ArrayProblem;
 
 class Driver{
     public static void main(String[] args) {
         ArrayProblem arrayProblem = new ArrayProblem();
         int arr[]={1,2,3,4,5,6,7,8,9};
-        arrayProblem.reverseArray(arr);
+
+        arrayProblem.kthElement(arr);
     }
 }
