0015403c0ee050c5bd9ec5df12c43414ae3357f9
ashwthama <ashwanikumarnt@gmail.com>
2021-11-06 20:47:43 +0530

    add function added

--- src/com/ashwani/main/Driver.java
+++ src/com/ashwani/main/Driver.java
@@ -3,5 +3,14 @@ package com.ashwani.main;
 class Driver{
     public static void main(String[] args) {
         System.out.println("Hello ashwani");
+        int n= add(5);
+        System.out.println(n);
+    }
+
+    public static int add(int n){
+        int a=10;
+        int b=20;
+        return a+b;
+
     }
 }
