ab8ec2d326e839a241671efe3079253d27689454
RISHI <therishikant@gmail.com>
2021-11-06 20:08:13 +0530

    ArrayProblem File Added

--- src/com/rishi/main/Driver.java
+++ src/com/rishi/main/Driver.java
@@ -1,3 +1,5 @@
+package com.rishi.main;
+
 class Driver{
     public static void main(String[] args) {
         System.out.println("Hello");
---
+++ src/com/rishi/utils/ArrayProblem.java
@@ -0,0 +1,7 @@
+package com.rishi.utils;
+
+public class ArrayProblem {
+    public void print(){
+        System.out.println("Printing Array");
+    }
+}
