46bac79fbb96053bb0b3ecab99cf8f7bb93f3b1d
ashwthama <ashwanikumarnt@gmail.com>
2021-11-07 21:23:42 +0530

    correction in kthElement method

--- src/com/ashwani/main/Driver.java
+++ src/com/ashwani/main/Driver.java
@@ -6,7 +6,7 @@ import com.ashwani.utils.ArrayProblem;
 class Driver{
     public static void main(String[] args) {
         ArrayProblem arrayProblem = new ArrayProblem();
-        int arr[]={2,1,3,4,5,7,6,9,8};
+        int arr[]={99,88,22,11,55,77,100,66,33};
 
         arrayProblem.kthElement(arr);
     }
